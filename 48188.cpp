cbfd09c5173c481e42b10c169beff567a8228f21
Rick-hunterr <109984246+Rick-hunterr@users.noreply.github.com>
2022-08-23 20:20:41 -0300

    Add files via upload

---
+++ 48188.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int numero;
+	char caracter;
+	float adlc = 4.814307;
+	double pdso = 94810542131542731142.4815411941171024609732;
+	
+	numero = 15;
+	caracter = 'a';
+	
+	printf("el valor del numero es : %i\nel valor del caracter es : %c\nel valor decimal es : %f\nel valor de a+p es :%f",numero,caracter,adlc,pdso);
+
+
+
+return 0;
+}
